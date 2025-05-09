c9010a7e5255acfa479de56e78f0d82d0143a2bb
Veri5ied <alvinuchenna14@gmail.com>
2025-05-09 14:03:45 +0100

    finished love story

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -7,6 +7,11 @@ import {
   MiniFlower,
   FirstMeet,
   Rule,
+  FirstDate,
+  MarriageProposal,
+  Engagement,
+  StoryFlowerOne,
+  StoryFlowerTwo,
 } from "../assets";
 import { CountdownTimer, WeddingAnnouncement } from "../components";
 
@@ -26,27 +31,27 @@ const loveStory: {
     position: "left",
   },
   {
-    title: "Our Fist Meet",
-    timeline: "Monday January 11th, 2019",
+    title: "Our First Date",
+    timeline: "Saturday January 18th, 2019",
     description:
-      "It all began with a simple moment—unexpected, yet unforgettable. Whether it was fate, coincidence, or a little bit of both, that day marked the beginning of everything. From strangers to something so much more, our journey started right there.",
-    img: FirstMeet,
+      "Nervous smiles, curious hearts, and that undeniable spark—our first date was the moment everything started to feel real. From the laughter we shared to the way time flew by, it was the beginning of something beautiful.",
+    img: FirstDate,
     position: "right",
   },
   {
-    title: "Our Fist Meet",
-    timeline: "Monday January 11th, 2019",
+    title: "Marriage Proposal",
+    timeline: "Friday January 18th, 2024",
     description:
-      "It all began with a simple moment—unexpected, yet unforgettable. Whether it was fate, coincidence, or a little bit of both, that day marked the beginning of everything. From strangers to something so much more, our journey started right there.",
-    img: FirstMeet,
+      "A question from the heart, a promise for a lifetime. In a moment filled with love, emotion, and happy tears, one simple “yes” changed everything. It was the start of forever, wrapped in love and sealed with a ring.",
+    img: MarriageProposal,
     position: "left",
   },
   {
-    title: "Our Fist Meet",
-    timeline: "Monday January 11th, 2019",
+    title: "Our Engagement",
+    timeline: "Wednesday December 29th, 2024",
     description:
-      "It all began with a simple moment—unexpected, yet unforgettable. Whether it was fate, coincidence, or a little bit of both, that day marked the beginning of everything. From strangers to something so much more, our journey started right there.",
-    img: FirstMeet,
+      "A celebration of love, commitment, and the beautiful journey ahead. Surrounded by joy and the people we love, our engagement marked the moment we officially said, “We’re in this together—forever.",
+    img: Engagement,
     position: "right",
   },
 ];
@@ -115,11 +120,11 @@ export default function Page() {
           <div className="grid lg:gap-36 gap-7 place-items-center relative">
             {loveStory.map(
               ({ title, timeline, description, img, position }, idx) => (
-                <div>
+                <div className="z-60">
                   {position === "left" ? (
                     <div
                       key={idx}
-                      className="flex flex-col items-center lg:flex-row gap-24"
+                      className="flex flex-col items-center lg:flex-row lg:gap-24 gap-12"
                     >
                       <div className="grid gap-2 max-w-[384px]">
                         <h1 className="text-7xl font-normal font-lovers-quarrel">
@@ -132,22 +137,26 @@ export default function Page() {
                           {description}
                         </p>
                       </div>
-                      <img
-                        src={img}
-                        alt={title}
-                        className="w-[332.93px] h-[332.93px] rounded-full"
-                      />
+                      <div className="border border-[#868686] rounded-full p-2">
+                        <img
+                          src={img}
+                          alt={title}
+                          className="w-[320px] h-[320px] rounded-full"
+                        />
+                      </div>
                     </div>
                   ) : (
                     <div
                       key={idx}
-                      className="flex flex-col-reverse items-center lg:flex-row gap-24"
+                      className="flex flex-col-reverse items-center lg:flex-row lg:gap-24 gap-12"
                     >
-                      <img
-                        src={img}
-                        alt={title}
-                        className="w-[332.93px] h-[332.93px] rounded-full"
-                      />
+                      <div className="border border-[#868686] rounded-full p-2">
+                        <img
+                          src={img}
+                          alt={title}
+                          className="w-[320px] h-[320px] rounded-full"
+                        />
+                      </div>
                       <div className="grid gap-2 max-w-[384px]">
                         <h1 className="text-7xl font-normal font-lovers-quarrel">
                           {title}
@@ -167,6 +176,16 @@ export default function Page() {
             <div className="hidden md:block lg:absolute -top-10 lg:-top-0">
               <img src={Rule} alt="Rule" />
             </div>
+            <img
+              src={StoryFlowerOne}
+              alt=""
+              className="absolute left-0 top-100"
+            />
+            <img
+              src={StoryFlowerTwo}
+              alt=""
+              className="absolute -right-0 bottom-[200]"
+            />
           </div>
         </div>
       </div>
