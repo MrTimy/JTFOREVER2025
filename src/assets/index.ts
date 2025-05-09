4f5c85804eb79986f6acfb1d4dc48e7a1df7865d
Veri5ied <alvinuchenna14@gmail.com>
2025-05-09 12:35:16 +0100

    hero section of landing page

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as Weddingpicone } from "./images/hero_one.png";
+export { default as Weddingpictwo } from "./images/hero_two.png";
+export { default as Weddingpicthree } from "./images/hero_three.png";
---
+++ src/components/ui/annoucement.tsx
@@ -0,0 +1,14 @@
+export default function WeddingAnnouncement() {
+  return (
+    <div className="bg-transparent border-4 border-white p-8 text-center text-white">
+      <h1 className="font-lovers-quarrel text-7xl mb-2">
+        #TinyAndJayyEverAfter
+      </h1>
+      <p className="my-6 text-2xl font-normal">WE ARE GETTING MARRIED!</p>
+      <hr className="border-t border-white my-6 mx-auto w-full" />
+      <p className="font-bold uppercase font-cairo text-2xl">
+        SATURDAY, 23RD JUNE 2025
+      </p>
+    </div>
+  );
+}
