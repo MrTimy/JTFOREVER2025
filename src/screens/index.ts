adbfa0a19a627ce3abedb6a8870fc3f07e129bcf
Veri5ied <alvinuchenna14@gmail.com>
2025-05-22 17:44:50 +0100

    programme page

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from "./home";
 export { default as Rsvp } from "./rsvp";
 export { default as Events } from "./events";
+export { default as Programme } from "./programme";
