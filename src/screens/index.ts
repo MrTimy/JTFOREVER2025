62199050e9adb049bfdd67e99f249d2228e90cb7
Veri5ied <alvinuchenna14@gmail.com>
2025-05-22 17:45:15 +0100

    gallery page

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from "./home";
 export { default as Rsvp } from "./rsvp";
 export { default as Events } from "./events";
 export { default as Programme } from "./programme";
+export { default as Gallery } from "./gallery";
